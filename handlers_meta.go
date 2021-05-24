51063c57996b41cbec4652242999e93de4a334e9
Usman Mughal <35332218+usmanmughalji@users.noreply.github.com>
2021-05-24 13:20:26 +0500

    url-fixing-4

--- handlers_meta.go
+++ handlers_meta.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/usmanmughalji/gdriveupload-sources/cli"
+	"github.com/usmanmughalji/gdriveupload-sources/tree/master/cli"
 	"os"
 	"runtime"
 	"strings"
